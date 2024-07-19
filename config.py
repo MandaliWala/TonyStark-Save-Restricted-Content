cdde55226c3f16cbfb08d8f1eec8a39643f2cd58
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-19 20:59:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6605759587:AAHLY42NBlfSSUhRJCGyk0ZYOcc4
 API_ID = int(os.environ.get("API_ID", "28530298"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "c042ac8604791a187b90be6684993e2b
-")
+API_HASH = os.environ.get("API_HASH", "c042ac8604791a187b90be6684993e2b")
 
 #Database 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
