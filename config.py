99e70a8343a6e201647b9d09ea8f7036db7a900b
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-05-23 09:03:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "28530298"))
 API_HASH = os.environ.get("API_HASH", "c042ac8604791a187b90be6684993e2b")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://fowardbot:fowardbot@cluster0.qjwunlh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://tabolo8539:0evqZDV4fC5fD17c@cluster0.cw8zxus.mongodb.net/?retryWrites=true&w=majority")
